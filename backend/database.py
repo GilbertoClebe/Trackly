004a65e15fc51cf9236a8d10da9380e425fd8d70
GILBERTO CLEBE DE SOUZA LIMA <gilberto_c_lima@estudante.sesisenai.org.br>
2026-05-19 10:18:40 -0300

    fix(database): base rename to Base.

--- backend/database.py
+++ backend/database.py
@@ -2,9 +2,9 @@ from sqlalchemy.orm import DeclarativeBase, sessionmaker
 from sqlalchemy import create_engine
 from dotenv import load_dotenv
 import os
-
 load_dotenv()
-class base(DeclarativeBase) :
+
+class Base(DeclarativeBase) :
     pass
 
 engine = create_engine(os.getenv("DATABASE_URL"))
