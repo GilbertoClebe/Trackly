134d47a58c8fb7cb451b5cf2bc3623e43d5f4a64
GILBERTO CLEBE DE SOUZA LIMA <gilberto_c_lima@estudante.sesisenai.org.br>
2026-05-21 12:59:20 -0300

    feat(service): goal service lógic construction.

---
+++ backend/services/goal_service.py
@@ -0,0 +1,37 @@
+from repository.goal_repository import GoalRepository
+from schemas.goal_schema import GoalCreate, GoalUpdate
+from models.goal_model import Goal
+class GoalService :
+    def __init__(self, repo: GoalRepository) :
+        self.repo = repo
+        
+    def create_goal(self, schema: GoalCreate) -> Goal :
+        goal = self.create_to_goal(schema)
+        return self.repo.create_goal(goal)
+    
+    def get_goal(self, id: int) -> Goal :
+        return self.repo.get_goal(id)
+    
+    def list_goals(self) -> list[Goal] :
+        return self.repo.list_goal()
+    
+    def update_goal(self, id: int, schema: GoalUpdate) -> Goal :
+        goal = self.update_to_goal(schema)
+        return self.repo.update_goal(id, goal)
+    
+    def delete_goal(self, id: int) -> None :
+        return self.repo.delete_goal(id)
+        
+    def create_to_goal(self, schema: GoalCreate) -> Goal :
+        return Goal(
+            status = schema.status.value,
+            title = schema.title,
+            description = schema.description
+        )
+        
+    def update_to_goal(self, schema: GoalUpdate) -> Goal :
+        return Goal(
+            status = schema.status.value,
+            title = schema.title,
+            description = schema.description
+        )
